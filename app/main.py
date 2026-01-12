e7b819558afe15a63b6bafcfa60603a303d1138e
Seroga <ilchuk.cergey@gmail.com>
2026-01-12 16:13:38 +0200

    added comprehension for creating list in create_person_list

--- app/main.py
+++ app/main.py
@@ -8,9 +8,7 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    list_of_persons = []
-    for person in people:
-        list_of_persons.append(Person(person["name"], person["age"]))
+    list_of_persons = [Person(person["name"], person["age"]) for person in people]
     for person in people:
         wife_name = person.get("wife")
         if wife_name is not None:
